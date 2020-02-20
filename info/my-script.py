c55d1b08e1c2a8b8a7784b7624a77701a1dfe0c9
Giuseppe Gambuzza <peppegambuzza@live.it>
2020-02-20 17:10:14 +0100

    nuovo test 2

--- info/my-script.py
+++ info/my-script.py
@@ -1,4 +1,5 @@
 import time
 
 i = 1
+i = 2
 print("this is the time NEW",time.time())
--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,7 @@ import time
 
 def my_dummy_function()
 	i = 1
+	i = 2
 	echo("This is my very dummy conda package")
 
 def my_very_function()
